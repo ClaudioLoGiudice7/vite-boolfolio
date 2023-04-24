bb3271b6c8ae7d35915e671d3e88fc937eaf561a
ClaudioLoGiudice7 <claudiologiudice6@gmail.com>
2023-04-24 20:32:25 +0200

    entering milestone 3 & 4

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script>
+
+// IMPORT DEL COMPONENTE
+import axios from "axios";
+import AppHeader from "./components/AppHeader.vue";
+
+// DEFINIZIONE DEL COMPONENTE IN PAGINA
+export default {
+  data() {
+    return {
+      title: "Hello!",
+      projects: [],
+    }
+  },
+
+  components: {
+    AppHeader
+  },
+
+  methods: {
+    fetchProjects() {
+      axios
+        .get("http://127.0.0.1:8000/api/projects")
+        .then((response) => {
+          this.projects = response.data;
+        })
+    }
+  },
+
+  created() {
+    this.fetchProjects();
+  }
+};
+</script>
+
+<template>
+  <AppHeader></AppHeader>
+  <h1>{{ title }}</h1>
+  <ul>
+    <li v-for="project in projects">{{ project.name }}</li>
+  </ul>
+</template>
+
+
+<style lang="scss"></style>
