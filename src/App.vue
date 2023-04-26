ec640390d5e141332aba3f27a42f1e23b417f94e
ClaudioLoGiudice7 <claudiologiudice6@gmail.com>
2023-04-26 16:50:03 +0200

    entering first 5 points of "Lato Vue"

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,25 @@
 <script>
 
 // IMPORT DEL COMPONENTE
-import axios from "axios";
 import AppHeader from "./components/AppHeader.vue";
-import ProjectList from "./components/ProjectList.vue";
 
 // DEFINIZIONE DEL COMPONENTE IN PAGINA
 export default {
   data() {
     return {
       title: "Vue Portfolio",
-      projects: {
-        list: [],
-        pages: [],
-      },
     }
   },
 
   components: {
-    AppHeader, ProjectList,
+    AppHeader
   },
-
-  emits: ["changePage"],
-
-  methods: {
-    fetchProjects(endpoint = null) {
-
-      if (!endpoint) endpoint = "http://127.0.0.1:8000/api/projects";
-
-      axios
-        .get(endpoint)
-        .then((response) => {
-          this.projects.list = response.data.data;
-          this.projects.pages = response.data.links;
-        })
-    }
-  },
-
-  created() {
-    this.fetchProjects();
-  }
 };
 </script>
 
 <template>
   <AppHeader :title="title"></AppHeader>
-  <ProjectList :projects="projects.list" :pages="projects.pages" title="Più recenti" class="my-3"
-    @changePage="fetchProjects"></ProjectList>
+  <router-view></router-view>
 </template>
 
 <style lang="scss"></style>
